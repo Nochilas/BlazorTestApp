8d575c20f183c430a04c73c4d8a5251c7f2335ae
Nochilas <patrononicholas@gmail.com>
2022-04-15 16:05:09 +0200

    Added Services, tried Dep. Inj.

---
+++ BlazorTestApp/Client/Services/RandomService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorTestApp.Client.Services
+{
+    public class RandomService
+    {
+        //Source: https://www.youtube.com/watch?v=gjvyM240B-Q&list=PLUOequmGnXxPrY79JGnF72e1Pba8z93zo&index=5
+        public static Guid RandomID { get; } = Guid.NewGuid();
+    }
+}
---
+++ BlazorTestApp/Client/Shared/ExampleChildComponentBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTestApp.Client.Shared
+{
+    public class ExampleChildComponentBase : ComponentBase
+    {
+		protected static bool _darkThemeOn;
+
+		protected static string Theme => _darkThemeOn ? "dark" : "light";
+
+		public string ThemeDescription { get; set; }
+
+		[Parameter]
+		public RenderFragment ChildContent { get; set; }
+
+		[Parameter]
+		public string ParentStuff { get; set; }
+
+		public void ChangeDescription()
+		{
+			_darkThemeOn = !_darkThemeOn;
+			Console.WriteLine(ThemeDescription);
+			ThemeDescription = $"The theme is {Theme}.";
+			Console.WriteLine(ThemeDescription);
+		}
+
+		//Trying methods override
+		protected override void OnInitialized()
+		{
+			_darkThemeOn = true;
+			ThemeDescription = $"The theme is {Theme}.";
+		}
+
+		//protected override void OnParametersSet()
+		//{
+		//	//Code
+		//}
+	}
+}
